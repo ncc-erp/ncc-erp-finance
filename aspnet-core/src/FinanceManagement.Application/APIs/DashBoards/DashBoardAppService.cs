159ca9a11798f738f575e6172de3bc65df148eb3
quan.daominh <quan.daominh@ncc.asia>
2025-04-18 10:59:59 +0700

    chore: Add export to Excel functionality for new chart data

--- aspnet-core/src/FinanceManagement.Application/APIs/DashBoards/DashBoardAppService.cs
+++ aspnet-core/src/FinanceManagement.Application/APIs/DashBoards/DashBoardAppService.cs
@@ -6,6 +6,7 @@ using ClosedXML.Excel;
 using DocumentFormat.OpenXml.Drawing.Charts;
 using DocumentFormat.OpenXml.Office2016.Drawing.ChartDrawing;
 using DocumentFormat.OpenXml.Office2016.Excel;
+using DocumentFormat.OpenXml.Packaging;
 using DocumentFormat.OpenXml.Spreadsheet;
 using FinanceManagement.APIs.ComparativeStatistics;
 using FinanceManagement.APIs.DashBoards.Dto;
@@ -35,6 +36,7 @@ using OfficeOpenXml.Style;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Drawing;
 using System.IO;
 using System.IO.Packaging;
 using System.Linq;
@@ -978,6 +980,101 @@ namespace FinanceManagement.APIs.DashBoards
                 }
             }
         }
+
+        [AbpAuthorize(PermissionNames.Dashboard)]
+        [HttpGet]
+        public async Task<byte[]> ExportDataNewChartToExcel([Required] DateTime startDate, [Required] DateTime endDate, bool isByPeriod)
+        {
+            ResultChartDto chartData = await GetNewChart(startDate, endDate, isByPeriod);
+            return GetByteDataExcelResultChartDto(chartData, startDate, endDate);
+        }
+
+        private byte[] GetByteDataExcelResultChartDto(ResultChartDto chartData, DateTime startDate, DateTime endDate)
+        {
+            var file = Helpers.GetInfoFileTemplate(new string[] { _env.WebRootPath, "Template_BaoCaoDoanhThuVaChiPhi.xlsx" });
+
+            using (var epck = new ExcelPackage(file.OpenRead()))
+            {
+                ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+                var sheet = epck.Workbook.Worksheets[0];
+
+                FillSheetWithChartData(ref sheet, chartData, startDate, endDate);
+
+                using (var stream = new MemoryStream())
+                {
+                    epck.SaveAs(stream);
+                    return stream.ToArray();
+                }
+            }
+        }
+
+
+        private void FillSheetWithChartData(ref ExcelWorksheet sheet, ResultChartDto chartData, DateTime startDate, DateTime endDate)
+        {
+            sheet.Cells["B3"].Value = startDate.ToString("dd/MM/yyyy");
+            sheet.Cells["E3"].Value = endDate.ToString("dd/MM/yyyy");
+
+            var labels = chartData.Labels.ToList();
+            var charts = chartData.Charts;
+
+            int startCol = 2;
+            int headerRow = 5;
+            int dataStartRow = 6;
+
+            for (int i = 0; i < labels.Count; i++)
+            {
+                var col = startCol + i;
+                var cell = sheet.Cells[headerRow, col];
+
+                cell.Value = labels[i];
+                cell.Style.Numberformat.Format = "@";
+                cell.Style.Fill.PatternType = ExcelFillStyle.Solid;
+                cell.Style.Fill.BackgroundColor.SetColor(ColorTranslator.FromHtml("#FFFF00"));
+            }
+
+            int totalCol = startCol + labels.Count;
+            var totalHeaderCell = sheet.Cells[headerRow, totalCol];
+            totalHeaderCell.Value = "TỔNG";
+            totalHeaderCell.Style.Fill.PatternType = ExcelFillStyle.Solid;
+            totalHeaderCell.Style.Fill.BackgroundColor.SetColor(ColorTranslator.FromHtml("#FFFF00"));
+
+            for (int i = 0; i < charts.Count; i++)
+            {
+                var chart = charts[i];
+                int row = dataStartRow + i;
+                bool isLastTwo = (i >= charts.Count - 2 && charts.Count > 2);
+
+                var nameCell = sheet.Cells[row, 1];
+                nameCell.Value = chart.Name;
+                if (isLastTwo) nameCell.Style.Font.Bold = true;
+
+                for (int j = 0; j < labels.Count; j++)
+                {
+                    var cell = sheet.Cells[row, startCol + j];
+                    cell.Value = chart.Data[j];
+                    cell.Style.Numberformat.Format = "#,##0";
+                    if (isLastTwo) cell.Style.Font.Bold = true;
+                }
+
+                var totalCell = sheet.Cells[row, totalCol];
+                totalCell.Value = chart.Data.Sum();
+                totalCell.Style.Numberformat.Format = "#,##0";
+                totalCell.Style.Font.Bold = true;
+            }
+
+            sheet.Cells[sheet.Dimension.Address].AutoFitColumns();
+
+            int lastDataRow = dataStartRow + charts.Count - 1;
+            var dataRange = sheet.Cells[headerRow, 1, lastDataRow, totalCol];
+            dataRange.Style.Border.Top.Style = ExcelBorderStyle.Thin;
+            dataRange.Style.Border.Bottom.Style = ExcelBorderStyle.Thin;
+            dataRange.Style.Border.Left.Style = ExcelBorderStyle.Thin;
+            dataRange.Style.Border.Right.Style = ExcelBorderStyle.Thin;
+        }
+
+
+
+
         [AbpAuthorize(PermissionNames.Dashboard)]
         [HttpPost]
         public async Task<List<ResultCircleChartDto>> GetCircleChart(InputListCircleChartDto input)
