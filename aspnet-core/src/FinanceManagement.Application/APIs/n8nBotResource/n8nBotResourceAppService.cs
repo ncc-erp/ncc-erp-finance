8bb3e93487ad7510dd9621b59594f51a270ab8f7
Trung Do <trung.dotrong@ncc.asia>
2025-07-16 16:08:49 +0700

    ThemReportTongChiN8N

---
+++ aspnet-core/src/FinanceManagement.Application/APIs/n8nBotResource/n8nBotResourceAppService.cs
@@ -0,0 +1,62 @@
+﻿using Abp.Application.Services;
+using FinanceManagement.GeneralModels;
+using FinanceManagement.IoC;
+using FinanceManagement.Managers.Dashboards;
+using Microsoft.AspNetCore.Mvc;
+using NccCore.Anotations;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using FinanceManagement.Authorization;
+using FinanceManagement.Entities;
+using FinanceManagement.Enums;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinanceManagement.APIs.N8nBotResource
+{
+    public class N8nBotResourceAppService : FinanceManagementAppServiceBase
+    {
+        private readonly IDashboardManager _dashboardManager;
+
+        public N8nBotResourceAppService(
+            IWorkScope workScope,
+            IDashboardManager dashboardManager
+        ) : base(workScope)
+        {
+            _dashboardManager = dashboardManager;
+        }
+
+        [HttpGet]
+        [NccAuth]
+        public async Task<BaoCaoChungDto> GetBaoCaoChungThangNay()
+        {
+            var now = DateTime.Now;
+            var startDate = new DateTime(now.Year, now.Month, 1);
+            var endDate = startDate.AddMonths(1).AddSeconds(-1);
+
+        
+            var dicCurrencyConvert = GetAndCheckDictionaryCurrencyConvertByYearMonth(startDate, endDate);
+
+            var result = await _dashboardManager.GetDataBaoCaoChung(
+                startDate,
+                endDate,
+                dicCurrencyConvert,
+                0,      // branchId = 0: lấy tất cả chi nhánh
+                null    // expenseType = null: không lọc theo loại chi
+            );
+
+
+            return result ?? new BaoCaoChungDto();
+        }
+
+        private Dictionary<CurrencyYearMonthDto, double> GetAndCheckDictionaryCurrencyConvertByYearMonth(DateTime startDate, DateTime endDate)
+        {
+            var dicCurrencyConvert = _dashboardManager.GetDictionaryCurrencyConvertByYearMonth(startDate, endDate);
+            _dashboardManager.CheckDictionaryCurrencyConvertByYearMonth(dicCurrencyConvert, startDate, endDate);
+            return dicCurrencyConvert;
+        }
+    }
+}
